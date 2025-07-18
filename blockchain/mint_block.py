d48a5655cdbbca2b96551b05d14b51fee05e4b77
AOK <armanomidikh@gmail.com>
2025-07-17 23:49:35 -0700

    Fix sys.path for GitHub Actions to locate hash_engine

--- blockchain/mint_block.py
+++ blockchain/mint_block.py
@@ -1,6 +1,15 @@
+import os
+import sys
+
+# This adds the root MetaCloudCoin directory to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
 from hash_engine.generate_metadata_hash import generate_hash
 import time
 import json
+
 
 block = {
     "timestamp": time.time(),
