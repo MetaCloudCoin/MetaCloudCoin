c2de93c823b20cca6bbcfea9fa423a2273c8a7f6
AOK <armanomidikh@gmail.com>
2025-07-19 00:40:31 -0700

    Commit all local changes before rebase

--- metadata/fetchers/aws_ip_ranges.py
+++ metadata/fetchers/aws_ip_ranges.py
@@ -1,6 +1,7 @@
 import requests
 
 def fetch_aws_ip_ranges():
-    url = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
+    url = "https://ip-ranges.amazonaws.com/ip-ranges.json"
     response = requests.get(url)
+    response.raise_for_status()
     return response.json()
